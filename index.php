98271197a04847c5974fefa2429c989dbcc8fd9a
Júnior Schrepel <junior.schrepel@hotmail.com>
2016-01-27 17:31:46 -0200

    Site Modelo - HTML5 / CSS3

---
+++ index.php
@@ -0,0 +1,164 @@
+<!document html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Primeiro Site</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="css/normalize.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/responsive.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+     
+    <?php /* include("slider.php");  */?>
+        
+        <!-- SECTION WELCOME -->
+        <section class="welcome">
+           <div class="container">
+               <article>
+                   <h2 class="font-title">Welcome to Zear</h2>
+                   <div class="divider-short"></div>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua ut enim ad minim veniam.</p>
+                </article>
+                <div class="row">
+                    <div class="column column-4">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <div class="icon"><i class="fa fa-lightbulb-o"></i></div>
+                                    <h4>Power Packed Design</h4>
+                                    <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.</p>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div class="column column-4">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <div class="icon"><i class="fa fa-diamond"></i></div>
+                                    <h4>Unlimited Possibilites</h4>
+                                    <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div class="column column-4">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <div class="icon"><i class="fa fa-life-ring"></i></div>
+                                    <h4>Top-Class Support & Updates</h4>
+                                    <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.</p>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        
+        <!-- SECTION TEAM -->
+        <section class="equipe">
+           <div class="container">
+               <h2 class="font-title">OUR TEAM</h2>
+               <div class="divider-short"></div>
+               
+               <div class="row">
+                   <div class="column column-3">
+                        <article>
+                            <figure><img src="img/team-1.jpg" alt="Team 1" /></figure>
+                            <h4 class="font-title">Jeff Walsh</h4>
+                            <p class="team-sub">CEO</p>
+                            <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula it ultricies purus, sed posuere libero
+                             id dui orci. Dui ligula it do ultricies purus, sed posuere libero id vel hendrerit interdum.</p>
+                            <nav class="social">
+                                <ul>
+                                    <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href=""><i class="fa fa-google"></i></a></li>
+                                    <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </nav>
+                        </article>
+                   </div>
+                   
+                   <div class="column column-3">
+                        <article>
+                            <figure><img src="img/team-2.jpg" alt="Team 1" /></figure>
+                            <h4>Janet Adams</h4>
+                            <p class="team-sub">Human Resources</p>
+                            <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula it ultricies purus, sed posuere libero
+                             id dui orci. Dui ligula it do ultricies purus, sed posuere libero id vel hendrerit interdum.</p>
+                            <nav class="social">
+                                <ul>
+                                    <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href=""><i class="fa fa-google"></i></a></li>
+                                    <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </nav>
+                        </article>
+                   </div>
+                   
+                   <div class="column column-3">
+                        <article>
+                            <figure><img src="img/team-3.jpg" alt="Team 1" /></figure>
+                            <h4>Colin Weiland</h4>
+                            <p class="team-sub">New Business Manager</p>
+                            <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula it ultricies purus, sed posuere libero
+                             id dui orci. Dui ligula it do ultricies purus, sed posuere libero id vel hendrerit interdum.</p>
+                            <nav class="social">
+                                <ul>
+                                    <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href=""><i class="fa fa-google"></i></a></li>
+                                    <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </nav>
+                        </article>
+                   </div>
+                   
+                   <div class="column column-3">
+                        <article>
+                            <figure><img src="img/team-4.jpg" alt="Team 1" /></figure>
+                            <h4>Wendy Willings</h4>
+                            <p class="team-sub">Public Relations</p>
+                            <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula it ultricies purus, sed posuere libero
+                             id dui orci. Dui ligula it do ultricies purus, sed posuere libero id vel hendrerit interdum.</p>
+                            <nav class="social">
+                                <ul>
+                                    <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href=""><i class="fa fa-google"></i></a></li>
+                                    <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </nav>
+                        </article>
+                   </div>
+                   
+               </div> <!-- Fim Row -->
+            </div>
+        </section>
+        
+        
+        <!-- / SECTION INFO02 -->
+        <section class="info02">
+            <div class="container">
+                <div class="features">
+                    <h2>Over 20 Years Experience</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip retex tempor incididunea commodo consequat.</p>
+                </div>
+                <div class="icon"><i class="fa fa-bar-chart"></i></div>
+            </div>
+        </section>
+        
+</body>
+
+</html>
---
+++ slider.php
@@ -0,0 +1,472 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+<!-- get jQuery from the google apis -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.js"></script>
+
+<!-- CSS STYLE-->
+<link rel="stylesheet" type="text/css" href="rev-slider-plugin/css/style.css" media="screen" />
+
+<!-- THE PREVIEW STYLE SHEETS, NO NEED TO LOAD IN YOUR DOM -->
+<!--<link rel="stylesheet" type="text/css" href="rev-slider-plugin/css/noneed.css" media="screen" />-->
+
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
+<script type="text/javascript" src="rev-slider-plugin/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+<script type="text/javascript" src="rev-slider-plugin/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+<!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+<link rel="stylesheet" type="text/css" href="rev-slider-plugin/css/extralayers.css" media="screen" />	
+<link rel="stylesheet" type="text/css" href="rev-slider-plugin/rs-plugin/css/settings.css" media="screen" />
+
+<!-- GOOGLE FONTS -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800' rel='stylesheet' type='text/css'>
+
+<!-- START REVOLUTION SLIDER 4.5.0 fullwidth mode -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:100,200,300,700,800,900' rel='stylesheet' type='text/css'>
+
+  
+
+  
+<article class="">
+
+<!-- ########### - BANNER - ############ -->
+	
+<div class="tp-banner-container">
+	<div class="tp-banner" >
+	    <ul>	
+        
+        
+        <!-- SLIDE 1 -->
+	    <li data-transition="fade" data-slotamount="7" data-masterspeed="500" data-thumb=""  data-saveperformance="on"  data-title="Slide 01">
+		<!-- MAIN IMAGE -->
+		<img src="img/dummy.png"  alt="slidebg1" data-lazyload="img/slidebg01.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+		<!-- LAYERS -->
+
+
+		<!-- LAYER NR. 1 -->
+		<div class="tp-caption black_heavy_60 sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="40"
+			data-y="110" 
+			data-speed="500"
+			data-start="500"
+			data-easing="Power3.easeInOut" 
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			style=" z-index: 7; 
+                    white-space: nowrap; 
+                    max-width: auto; 
+			        max-height: auto; 
+			        line-height: 60px;
+			        margin: 0px;
+			        padding: 0px;
+			        letter-spacing: 1px;
+			        font-size: 47px;
+			        font-weight: 700;
+			        left: 34px;
+			        top: 170px;
+			        visibility: visible;
+			        opacity: 1;
+			        text-transform:uppercase;">It is easy and fun to<br>create stunning websites
+		</div>
+
+		<!-- LAYER NR. 2 -->
+		<div class="tp-caption black_thin_34  sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="40"
+			data-y="240" 
+			data-customin="x:0;y:0;z:0;rotationX:90;rotationY:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+			data-speed="500"
+			data-start="1000"
+			data-easing="Back.easeOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			style="z-index: 6; 
+                   white-space: nowrap;
+			       max-width: auto; 
+			       max-height: auto; 
+			       line-height: 25px;
+			       margin: 0px;
+			       padding: 0px;
+			       letter-spacing: 0px;
+			       font-size: 20px;
+			       font-weight: 500;
+			       left: 34px;
+			       top: 307;
+			       visibility: visible;">Clean, Cretive and Powerful Multipurpose for Web
+		</div>
+
+		<!-- LAYER NR. 3 -->
+		<div class="tp-caption sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="40"
+			data-y="310" 
+			data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+			data-speed="500"
+			data-start="1500"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-linktoslide="next"
+			style="z-index: 12; 
+                  white-space: nowrap;
+			      max-width: auto; 
+			      max-height: auto;"><a href='#' class='btn-custom-radius'>Read More</a>
+		</div>
+		
+		<!-- LAYER NR. 4 -->
+		<div class="tp-caption sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="210"
+			data-y="310" 
+			data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+			data-speed="500"
+			data-start="1500"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-linktoslide="next"
+			style="z-index: 12; 
+                  white-space: nowrap;
+			      max-width: auto; 
+			      max-height: auto;"><a href='#' class='btn-custom-border-black-radius'>Contact Us</a>
+		</div>
+	</li>
+	
+	
+	
+	<!-- SLIDE 2 -->
+	<li data-transition="slideleft" data-slotamount="7" data-masterspeed="2000" data-thumb="" data-delay="10000"  data-saveperformance="on"  data-title="Slide 02">
+		<!-- MAIN IMAGE -->
+		<img src="img/dummy.png"  alt="sliderbg02" data-lazyload="img/slidebg02.jpg" data-bgposition="center" data-bgfit="cover" data-bgrepeat="no-repeat">
+		<!-- LAYERS -->
+        
+
+        <!-- LAYER NR. 1 -->
+		<div class="tp-caption black_heavy_60 sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="40"
+			data-y="110" 
+			data-speed="500"
+			data-start="2000"
+			data-easing="Power3.easeInOut" 
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			style=" z-index: 7; 
+                    white-space: nowrap; 
+                    max-width: auto; 
+			        max-height: auto; 
+			        line-height: 60px;
+			        margin: 0px;
+			        padding: 0px;
+			        letter-spacing: 1px;
+			        font-size: 47px;
+			        font-weight: 700;
+			        color: #fff;
+			        left: 34px;
+			        top: 170px;
+			        visibility: visible;
+			        opacity: 1;
+			        text-transform:uppercase;">It is easy and fun to<br>create stunning websites
+		</div>
+
+		<!-- LAYER NR. 2 -->
+		<div class="tp-caption black_thin_34  sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="40"
+			data-y="240" 
+			data-customin="x:0;y:0;z:0;rotationX:90;rotationY:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+			data-speed="500"
+			data-start="2500"
+			data-easing="Back.easeOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			style="z-index: 6; 
+                   white-space: nowrap;
+			       max-width: auto; 
+			       max-height: auto; 
+			       line-height: 25px;
+			       margin: 0px;
+			       padding: 0px;
+			       letter-spacing: 0px;
+			       font-size: 20px;
+			       font-weight: 500;
+			       color: #fff;
+			       left: 34px;
+			       top: 307;
+			       visibility: visible;">Clean, Cretive and Powerful Multipurpose for Web
+		</div>
+
+		<!-- LAYER NR. 3 -->
+		<div class="tp-caption sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="40"
+			data-y="310" 
+			data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+			data-speed="500"
+			data-start="3000"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-linktoslide="next"
+			style="z-index: 12; 
+                  white-space: nowrap;
+			      max-width: auto; 
+			      max-height: auto;"><a href='#' class='btn-custom-radius'>Read More</a>
+		</div>
+		
+		<!-- LAYER NR. 4 -->
+		<div class="tp-caption sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="210"
+			data-y="310" 
+			data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+			data-speed="500"
+			data-start="3000"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-linktoslide="next"
+			style="z-index: 12; 
+                  white-space: nowrap;
+			      max-width: auto; 
+			      max-height: auto;"><a href='#' class='btn-custom-border-white-radius'>Contact Us</a>
+		</div>
+	</li>
+	
+	
+	
+	<!-- SLIDE 3 -->
+	<li data-transition="slideup" data-slotamount="7" data-masterspeed="1000" data-thumb=""  data-saveperformance="on"  data-title="Slide 03">
+		<!-- MAIN IMAGE -->
+		<img src="img/dummy.png"  alt="slidebg03" data-lazyload="img/slidebg03.jpg" data-bgposition="center" data-bgfit="cover" data-bgrepeat="no-repeat">
+		<!-- LAYERS -->
+
+
+        <!-- LAYER NR. 1 -->
+		<div class="tp-caption black_heavy_60 sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="right"
+			data-y="110" 
+			data-speed="500"
+			data-start="2000"
+			data-easing="Power3.easeInOut" 
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			style=" z-index: 7; 
+                    white-space: nowrap; 
+                    max-width: auto; 
+			        max-height: auto; 
+			        line-height: 60px;
+			        margin-right: 40px;
+			        padding: 0px;
+			        letter-spacing: 1px;
+			        font-size: 47px;
+			        font-weight: 700;
+			        top: 170px;
+			        visibility: visible;
+			        opacity: 1;
+			        text-transform:uppercase;">It is easy and fun to
+		</div>
+		
+		<!-- LAYER NR. 2 -->
+		<div class="tp-caption black_heavy_60 sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="right"
+			data-y="180" 
+			data-speed="500"
+			data-start="2000"
+			data-easing="Power3.easeInOut" 
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			style="z-index: 6; 
+                   white-space: nowrap;
+			       max-width: auto; 
+			       max-height: auto; 
+			       line-height: 25px;
+			       margin-right: 40px;
+			       padding: 0px;
+			       letter-spacing: 0px;
+			       font-size: 47px;
+			       font-weight: 700;
+			       top: 307;
+			       visibility: visible;
+			       text-transform:uppercase;">Create stunning websites
+		</div>
+
+		<!-- LAYER NR. 3 -->
+		<div class="tp-caption black_thin_34  sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="right"
+			data-y="240" 
+			data-customin="x:0;y:0;z:0;rotationX:90;rotationY:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
+			data-speed="500"
+			data-start="2500"
+			data-easing="Back.easeOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			style="z-index: 6; 
+                   white-space: nowrap;
+			       max-width: auto; 
+			       max-height: auto; 
+			       line-height: 25px;
+			       margin-right: 40px;
+			       padding: 0px;
+			       letter-spacing: 0px;
+			       font-size: 20px;
+			       font-weight: 500;
+			       left: 34px;
+			       top: 307;
+			       visibility: visible;">Clean, Cretive and Powerful Multipurpose for Web
+		</div>
+
+		<!-- LAYER NR. 4 -->
+		<div class="tp-caption sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="right"
+			data-y="310" 
+			data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+			data-speed="500"
+			data-start="3000"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-linktoslide="next"
+			style="z-index: 12; 
+                  white-space: nowrap;
+			      max-width: auto; 
+			      max-height: auto;
+			      margin-right: 220px;"><a href='#' class='btn-custom-radius'>Read More</a>
+		</div>
+		
+		<!-- LAYER NR. 5 -->
+		<div class="tp-caption sfb tp-resizeme rs-parallaxlevel-0"
+			data-x="right"
+			data-y="310" 
+			data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+			data-speed="500"
+			data-start="3000"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-linktoslide="next"
+			style="z-index: 12; 
+                  white-space: nowrap;
+			      max-width: auto; 
+			      max-height: auto;
+			      margin-right: 40px;"><a href='#' class='btn-custom-border-black-radius'>Contact Us</a>
+		</div>
+	</li>
+</ul>
+
+<div class="tp-bannertimer"></div>	</div>
+</div>	
+</article>		
+			<script type="text/javascript">
+
+				jQuery(document).ready(function() {
+				
+					
+								
+					jQuery('.tp-banner').show().revolution(
+					{
+						dottedOverlay:"none",
+						delay:16000,
+						startwidth:1170,
+						startheight:500,
+						hideThumbs:200,
+						
+						thumbWidth:100,
+						thumbHeight:50,
+						thumbAmount:5,
+						
+						navigationType:"bullet",
+						navigationArrows:"solo",
+						navigationStyle:"preview1",
+						
+						touchenabled:"on",
+						onHoverStop:"on",
+						
+						swipe_velocity: 0.7,
+						swipe_min_touches: 1,
+						swipe_max_touches: 1,
+						drag_block_vertical: false,
+												
+												parallax:"mouse",
+						parallaxBgFreeze:"on",
+						parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
+												
+						keyboardNavigation:"off",
+						
+						navigationHAlign:"center",
+						navigationVAlign:"bottom",
+						navigationHOffset:0,
+						navigationVOffset:20,
+
+						soloArrowLeftHalign:"left",
+						soloArrowLeftValign:"center",
+						soloArrowLeftHOffset:20,
+						soloArrowLeftVOffset:0,
+
+						soloArrowRightHalign:"right",
+						soloArrowRightValign:"center",
+						soloArrowRightHOffset:20,
+						soloArrowRightVOffset:0,
+								
+						shadow:0,
+						fullWidth:"on",
+						fullScreen:"off",
+
+						spinner:"spinner4",
+						
+						stopLoop:"off",
+						stopAfterLoops:-1,
+						stopAtSlide:-1,
+
+						shuffle:"off",
+						
+						autoHeight:"off",						
+						forceFullWidth:"off",						
+												
+												
+												
+						hideThumbsOnMobile:"off",
+						hideNavDelayOnMobile:1500,						
+						hideBulletsOnMobile:"off",
+						hideArrowsOnMobile:"off",
+						hideThumbsUnderResolution:0,
+						
+						hideSliderAtLimit:0,
+						hideCaptionAtLimit:0,
+						hideAllCaptionAtLilmit:0,
+						startWithSlide:0,
+						videoJsPath:"rs-plugin/videojs/",
+						fullScreenOffsetContainer: ""	
+					});
+					
+					
+					
+									
+				});	//ready
+				
+			</script>
+			
+<!-- END REVOLUTION SLIDER -->		
+	</div>
+</div>
+
